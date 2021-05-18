7047238f7fbf0d88738d3e30f693b537a30939b3
lemon-shark <shiqiaozhu@hotmail.com>
2021-05-18 17:36:28 -0400

    todolist page

--- editit_project/urls.py
+++ editit_project/urls.py
@@ -17,8 +17,13 @@ from django.contrib import admin
 from django.urls import path
 
 from hello.views import myView
+from todo.views import todoView, addTodo, deleteTodo
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sayHello/', myView),
+    path('todo/', todoView),
+    path('addTodo/', addTodo),
+    path('deleteTodo/<int:todo_id>/', deleteTodo),
 ]
