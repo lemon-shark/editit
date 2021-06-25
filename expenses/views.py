e08f2cd671bf2af6176682fbf567cd2c261e32ec
lemon-shark <shiqiaozhu@hotmail.com>
2021-06-24 21:06:22 -0400

    add expenses form and its front-end validation

--- expenses/views.py
+++ expenses/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Category, Expense
+from django.contrib import messages
 
 
+@login_required(login_url='/authentication/loginnew')
 def index(request):
+    categoties = Category.objects.all()
     return render(request, 'expenses/index.html')
 
 
 def add_expense(request):
-    return render(request, 'expenses/add_expense.html')
+    categoties = Category.objects.all()
+    context = {
+        'categories': categoties,
+        'values': request.POST
+    }
+
+    if request.method == 'GET':
+        return render(request, 'expenses/add_expense.html', context)
+
+    if request.method == 'POST':
+        amount = request.POST['amount']
+
+        if not amount:
+            messages.error(request, 'Amount is required')
+            return render(request, 'expenses/add_expense.html', context)
+
+        description = request.POST['description']
+
+        if not description:
+            messages.error(request, 'Description is required')
+            return render(request, 'expenses/add_expense.html', context)
