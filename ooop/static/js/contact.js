69d68fb886b4de357feae58f792df130768103fd
lemon-shark <shiqiaozhu@hotmail.com>
2021-10-08 10:16:33 -0700

    move admin static files

---
+++ ooop/static/js/contact.js
@@ -0,0 +1,4 @@
+const descriptionField= document.querySelector('#descriptionField');
+const descriptionFeedbackArea= document.querySelector('.description_feedback');
+const submitBtn = document.querySelector('.submit-btn');
+
