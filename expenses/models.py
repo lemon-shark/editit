e3e541f5b7674376a14d8f7b171ba01264f1e94f
lemon-shark <shiqiaozhu@hotmail.com>
2021-10-18 09:09:44 -0700

    remove kind field

--- expenses/models.py
+++ expenses/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.utils.timezone import now
 from django.contrib.auth.models import User
 from django.conf import settings
+import csv
+from django.http import HttpResponse
+from io import StringIO
+from django.contrib import admin
+from csvexport.actions import csvexport
 
 
 class Expense(models.Model):
@@ -11,7 +16,6 @@ class Expense(models.Model):
     #     owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
     owner = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     category = models.CharField(max_length=266)
-    kind = models.BooleanField(default=False)
 
     def __str__(self):
         return self.category
@@ -28,3 +32,23 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class StatAdmin(admin.ModelAdmin):
+    actions = [csvexport]
+    # list_display = ('amount', 'date', 'description', 'owner', 'category')
+    #
+    # def download_csv(self, request, queryset):
+    #     f = StringIO.StringIO()
+    #     writer = csv.writer(f)
+    #     writer.writerow(["amount", "date", "description", "owner", "category"])
+    #
+    #     for s in queryset:
+    #         writer.writerow([s.code, s.country, s.ip, s.url, s.count])
+    #
+    #     f.seek(0)
+    #     response = HttpResponse(f, content_type='text/csv')
+    #     response['Content-Disposition'] = 'attachment; filename=stat-info.csv'
+    #     return response
+    #
+    # download_csv.short_description = "Download CSV file for selected stats."
